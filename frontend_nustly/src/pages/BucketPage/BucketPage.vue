5071eb3f0ada3eafd23140d89757548d1400016c
Artem <artemkholev@yandex.ru>
2023-12-11 23:19:38 +0300

    find problem one deploy

--- frontend_nustly/src/pages/BucketPage/BucketPage.vue
+++ frontend_nustly/src/pages/BucketPage/BucketPage.vue
@@ -48,4 +48,4 @@ onMounted(() => {
 });
 </script>
 
-<style src="./BucketPage.style.scss" lang="scss" scoped></style>../../entities/BucketList/BucketList.vue
+<style src="./BucketPage.style.scss" lang="scss" scoped></style>

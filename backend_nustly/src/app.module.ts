233716a21d813a35dce5a73b8b1119d0477bd075
Artem <artemkholev@yandex.ru>
2023-12-12 14:01:52 +0300

    connect with postgresql

--- backend_nustly/src/app.module.ts
+++ backend_nustly/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'artem',
+      password: 'TV',
+      database: 'Nustly',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}

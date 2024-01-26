22c2d6218f8e0c51e001a969f2976ea750b2134b
Artem <artemkholev@yandex.ru>
2024-01-26 22:23:38 +0300

    add function refreshTolen, edit .env

--- frontend_nustly/src/shered/api/index.ts
+++ frontend_nustly/src/shered/api/index.ts
@@ -1,4 +1,8 @@
 import axios, { type AxiosRequestConfig } from 'axios';
+import type { IResponseLogin } from './authApi/authApi.types';
+import { useAuthStore } from '../store/auth';
+import { storeToRefs } from 'pinia';
+import { decodeJwt } from '../jwtRequest/decodeJwt';
 
 
 export const API_URL = import.meta.env.VITE_BASE_URL;
@@ -12,6 +16,27 @@ const apiAxios = axios.create({
 apiAxios.interceptors.request.use((config: AxiosRequestConfig | any) => {
   config.headers!.Authorization = `Bearer ${sessionStorage.getItem('accessToken')}`;
   return config;
+});
+
+apiAxios.interceptors.response.use((config) => config, async (error) => {
+  const originalRequest = error.config;
+
+  if (error.response.status === 401 && error.config && !error.config._isRetry) {
+    originalRequest._isRetry = true;
+    try {
+      const response = await axios.get<IResponseLogin>(`${API_URL}/refresh`, { withCredentials: true });
+      const authStore = useAuthStore();
+      const { userName } = storeToRefs(authStore);
+
+      sessionStorage.setItem('accessToken', response.data.accessToken);
+      userName.value = decodeJwt(response.data.accessToken).email;
+
+      return apiAxios.request(originalRequest);
+    } catch (e:any) {
+      console.log(e.message);
+    }
+  }
+  throw error;
 });
 
 export default apiAxios;

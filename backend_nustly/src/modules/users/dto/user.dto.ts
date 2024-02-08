9591fb5defcc83d2db9a010e77742644bfb66332
Artem <artemkholev@yandex.ru>
2024-02-08 22:35:57 +0300

    add in access token role

--- backend_nustly/src/modules/users/dto/user.dto.ts
+++ backend_nustly/src/modules/users/dto/user.dto.ts
@@ -1,10 +1,12 @@
 class User {
   id;
   email;
+  roles;
 
   constructor(model) {
     this.id = model.id;
     this.email = model.email;
+    this.roles = model.roles[0].dataValues.value;
   }
 }
 

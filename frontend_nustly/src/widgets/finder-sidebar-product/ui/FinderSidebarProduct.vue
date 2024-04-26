a78cb8109c8a91f02ea3db277174386ad39ec3c7
Artem <artemkholev@yandex.ru>
2024-04-27 01:34:30 +0300

    add elems side-bar products

--- frontend_nustly/src/widgets/finder-sidebar-product/ui/FinderSidebarProduct.vue
+++ frontend_nustly/src/widgets/finder-sidebar-product/ui/FinderSidebarProduct.vue
@@ -7,7 +7,32 @@
       <span @click="$emit('close', $event)">×</span>
     </div>
     <div class="side-bar__container">
-
+      <div style="display: flex; justify-content: space-between;">
+        <p style="font-size: 30px;">Цена:</p>
+        <button style="border: none; border-radius: 5px; padding: 10px; cursor: pointer;">Очистить</button>
+      </div>
+      
+      <div style="display: flex; padding: 20px 0; align-items: center;">
+        <input style="width: 40%; height: 40px; border-radius: 5px;" type="number">
+        <span style="width: 5%; height: 1px; color: black; border: 1px solid black; margin: 0 20px;"></span>
+        <input style="width: 40%; height: 40px; border-radius: 5px;" type="number">
+      </div>
+    </div>
+    <div class="side-bar__footer">
+      <button-elem
+        :clName="null"
+        :title="'Применить'"
+        :handler="() => {}"
+        :width="'350px'"
+        :height="'55px'"
+        :background="'orangered'"
+        :textColor="null"
+        :fontSize="null"
+        :fontWeight="null"
+        :margin="'0 0 0 0'"
+        :borderRadius="'1px'"
+        :icon="null"
+      />
     </div>
   </div>
 </template>
